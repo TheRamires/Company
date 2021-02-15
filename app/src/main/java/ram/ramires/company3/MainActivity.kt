95b7b2e4107f8cd8b6c6d0c67e38f751a612e467
exercise <theendorfin@mail.ru>
2021-02-16 00:19:28 +0300

    #

--- app/src/main/java/ram/ramires/company3/MainActivity.kt
+++ app/src/main/java/ram/ramires/company3/MainActivity.kt
@@ -2,20 +2,14 @@ package ram.ramires.company3
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 import java.util.*
+import androidx.databinding.DataBindingUtil.setContentView
+import ram.ramires.company3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         Locale.setDefault(Locale("ru", "RU"))
-
-        val navHostFragment=
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController: NavController=navHostFragment.navController
-
+        setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
